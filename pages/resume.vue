ff6063cd16a989cfffce021b6765b3c9d1fa8ef5
Elijah Johnson <astroboy772@gmail.com>
2024-05-02 19:40:11 -0700

    resume2

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
       <div class="container">
-        <section>
-          <NuxtImg src="/resume1.jpg"></NuxtImg>
+        <section class="">
+          <NuxtImg src="/resume1.jpg" class="mx-auto my-auto"></NuxtImg>
+          <NuxtImg src="/resume2.jpg" class="mx-auto my-auto"></NuxtImg>
         </section>
 
         <section>
