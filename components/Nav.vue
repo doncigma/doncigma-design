caae2ea8d67540c13e94392822f590725e684e61
Elijah Johnson <astroboy772@gmail.com>
2024-05-06 21:41:06 -0700

    nav done and dusted

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,43 +2,40 @@
   <header class="bg-slate-50 dark:bg-black">
     
     <!-- Whole Nav bar element
-    items-center px-8 pb-4 pt-6 md:flex-row  md:space-x-12 md:px-12 -->
-    <div
-      class="container mx-auto flex flex-row items-center px-8 pb-4 pt-4 "
+    items-center px-4 pb-4 pt-6 md:flex-row  md:space-x-12 md:px-12 -->
+    <div class="container mx-auto flex flex-row items-center 
+        justify-center md:justify-normal space-x-3 px-2 pb-2 pt-2"
     >
       <!-- Logo words -->
-      <div class="flex text-center md:w-2/5">
-        <div class="text-sm md:text-lg font-extrabold md:w-fit md:text-left">
-          <span class="text-mexicanred-500 active:text-mexicanred-300 
-            md:hover:text-mexicanred-300">Doncigma</span>
-          <span class="dark:text-slate-400">Design</span>
+      <div class="md:w-1/2">
+        <div class="text-sm md:text-2xl font-extrabold md:w-fit md:text-left">
+          <span class="text-mexicanred-500 md:hover:text-mexicanred-300">Doncigma</span>
+          <span class="text-slate-400">Design</span>
         </div>
       </div>
-
+      
       <!-- Index link -->
-      <div class="text-center text-slate-400 md:w-1/5 md:text-left">
-        <nuxt-link class="text-md font-semibold" to="/">Home</nuxt-link>
+      <div class="text-slate-400 md:w-1/2">
+        <nuxt-link class="text-xs md:text-base font-semibold" to="/">Home</nuxt-link>
       </div>
 
       <!-- About me link -->
-      <div class="flex text-center text-slate-400 md:w-1/5 md:text-left">
-        <nuxt-link class="text-md font-semibold" to="/about">About Me</nuxt-link>
+      <div class="text-slate-400 md:w-1/2">
+        <nuxt-link class="text-xs md:text-base font-semibold" to="/about">About Me</nuxt-link>
       </div>
       
       <!-- Portfolio link -->
-      <div class="text-center text-slate-400 md:w-1/5 md:text-left">
-        <nuxt-link class="text-md font-semibold" to="/portfolio">Portfolio</nuxt-link>
+      <div class="text-slate-400 md:w-1/2">
+        <nuxt-link class="text-xs md:text-base font-semibold" to="/portfolio">Portfolio</nuxt-link>
       </div>
       
       <!-- Resume link -->
-      <div class="text-center text-slate-400 md:w-1/5 md:text-left">
-        <nuxt-link class="text-md font-semibold" to="/resume">Resume</nuxt-link>
+      <div class="text-slate-400 md:w-1/2">
+        <nuxt-link class="text-xs md:text-base font-semibold" to="/resume">Resume</nuxt-link>
       </div>
     </div>
     
-    <div
-      class="container mx-auto border-t border-slate-200 p-3 text-center text-sm text-slate-600 dark:border-slate-900 md:flex-row md:px-12"
-    >
-    </div>
+    <!-- Bottom line -->
+    <div class="container mx-auto border-t p-3 border-slate-900 md:px-12"/>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex flex-col bg-black pb-8">
-        <p class="flex justify-center text-center text-5xl text-slate-400">Welcome to</p>
-        <p class="flex justify-center text-center text-6xl text-mexicanred-500 font-semibold">Doncigma</p>
-        <p class="flex justify-center text-center text-5xl text-slate-400">Design.</p>
+        <p class="flex justify-center text-center text-5xl text-slate-500">Welcome to</p>
+        <p class="flex justify-center text-center text-6xl text-mexicanred-500">Doncigma</p>
+        <p class="flex justify-center text-center text-5xl text-slate-400 font-semibold">Design.</p>
     </div>
 
     <!-- Middle element -->
-    <div class="flex flex-col md:items-stretch md:flex-row">
-        <div class="flex md:mr-auto bg-blue-500">
+    <div class="flex flex-col md:flex-row md:items-center ">
+        <div class="bg-blue-500">
             <NuxtImg class="object-fill" src="/stock-red.jpg"/>
         </div>
 
-        <div class="flex bg-blue-500 md:mx-44">
+        <div class="bg-blue-500 md:mx-44">
             <p class="bg-red-500 my-auto md:mx-44 text-3xl">
                 Doncigma Design provides custom web design and development services 
                 to help you create a website that is unique, modern, and
@@ -19,7 +19,7 @@
             </p>
         </div>
 
-        <div class="flex flex-col md:mb-96 md:flex-row md:ml-auto">
+        <div class="md:mb-96 md:ml-auto">
             <NuxtImg src="/red-laptop.jpg"/>
         </div>
     </div>
