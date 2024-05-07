35652ff2f0dde80183c23d53996f870bcc0fa4b8
Elijah Johnson <astroboy772@gmail.com>
2024-05-06 23:25:57 -0700

    about outline

--- pages/about.vue
+++ pages/about.vue
@@ -4,17 +4,17 @@
         <div class="flex flex-col">
             <!-- About Me -->
             <div>
-                
+                quick credentials
             </div>
 
             <!-- Skills -->
             <div>
-                
+                C++ for 2 years etc
             </div>
 
             <!-- Hobbies -->
             <div>
-
+                backpacking, cars, warhammer, reading, poetry etc
             </div>
         </div>
     </div>
