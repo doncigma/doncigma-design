4286406ca87b56d693e0d8e64df3a7c22a60ee79
Elijah Johnson <astroboy772@gmail.com>
2024-05-06 22:32:13 -0700

    home page mobile design

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,32 @@
         <p class="flex justify-center text-center text-5xl text-slate-400 font-semibold">Design.</p>
     </div>
 
-    <!-- Middle element -->
-    <div class="flex flex-col md:flex-row md:items-center ">
+    <!-- Company description -->
+    <div class="flex flex-col md:flex-row md:items-center bg-gradient-to-l from-slate-900 to-mexicanred-500">
         <div class="bg-blue-500">
             <NuxtImg class="object-fill" src="/stock-red.jpg"/>
         </div>
-
-        <div class="bg-blue-500 md:mx-44">
-            <p class="bg-red-500 my-auto md:mx-44 text-3xl">
-                Doncigma Design provides custom web design and development services 
-                to help you create a website that is unique, modern, and
-                most importantly, a representation of you. Put the best of you first, Doncigma Design.
+        <!-- bg-gradient-to-t from-mexicanred-500 to-slate-900 -->
+        <div class="m-2 bg-black text-left p-3">
+            <p class="bg-black text-xl text-slate-400 text-justify 
+                p-4 border-2 border-slate-800"
+            >
+                <span class="text-mexicanred-500">Doncigma</span>
+                <span class="text-slate-300">Design</span> 
+                provides 
+                <span class="text-slate-200 font-semibold">custom web design </span>
+                and 
+                <span class="text-slate-200 font-semibold">development services</span> 
+                to help you create a website that is 
+                <span class="text-slate-200 font-semibold">unique</span>, 
+                <span class="text-slate-200 font-semibold">modern</span>, 
+                and most importantly, a 
+                <span class="text-slate-200 font-semibold">representation of you</span>. 
+                
+                <br><br>
+                Put the best part of you forward,
+                <span class="text-mexicanred-500">Doncigma</span>
+                <span class="text-slate-300">Design</span>.
             </p>
         </div>
 
