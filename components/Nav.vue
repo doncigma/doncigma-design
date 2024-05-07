d3623bfdb20eb3e81369b7a300b30e03d0132d34
Elijah Johnson <astroboy772@gmail.com>
2024-05-06 23:14:37 -0700

    nav formatting

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,41 +1,41 @@
 <template>
-  <header class="bg-black">
+    <header class="bg-black">
     
-    <!-- Whole Nav bar element
-    items-center px-4 pb-4 pt-6 md:flex-row  md:space-x-12 md:px-12 -->
-    <div class="container mx-auto flex flex-row items-center 
-        justify-center space-x-3 px-2 pb-2 pt-2"
-    >
-      <!-- Logo words -->
-      <div class="md:w-1/2">
-        <div class="text-md md:text-2xl font-extrabold">
-          <span class="text-mexicanred-500 md:hover:text-mexicanred-300">Doncigma</span>
-          <span class="text-slate-400">Design</span>
-        </div>
-      </div>
-      
-      <!-- Index link -->
-      <div class="text-slate-400 md:w-1/2">
-        <nuxt-link class="text-xs md:text-base font-semibold" to="/">Home</nuxt-link>
-      </div>
+        <!-- Whole Nav bar element
+        items-center px-4 pb-4 pt-6 md:flex-row  md:space-x-12 md:px-12 -->
+        <div class="container mx-auto flex flex-row items-center 
+            justify-center space-x-3 px-2 pb-2 pt-2"
+        >
+            <!-- Logo words -->
+            <div class="md:w-1/2">
+                <div class="text-md md:text-2xl font-extrabold">
+                    <span class="text-mexicanred-500 md:hover:text-mexicanred-300">Doncigma</span>
+                    <span class="text-slate-400">Design</span>
+                </div>
+            </div>
+        
+            <!-- Index link -->
+            <div class="text-slate-400 md:w-1/2">
+                <nuxt-link class="text-xs md:text-base font-semibold" to="/">Home</nuxt-link>
+            </div>
 
-      <!-- About me link -->
-      <div class="text-slate-400 md:w-1/2">
-        <nuxt-link class="text-xs md:text-base font-semibold" to="/about">About Me</nuxt-link>
-      </div>
-      
-      <!-- Portfolio link -->
-      <div class="text-slate-400 md:w-1/2">
-        <nuxt-link class="text-xs md:text-base font-semibold" to="/portfolio">Portfolio</nuxt-link>
-      </div>
-      
-      <!-- Resume link -->
-      <div class="text-slate-400 md:w-1/2">
-        <nuxt-link class="text-xs md:text-base font-semibold" to="/resume">Resume</nuxt-link>
-      </div>
-    </div>
+            <!-- About me link -->
+            <div class="text-slate-400 md:w-1/2">
+                <nuxt-link class="text-xs md:text-base font-semibold" to="/about">About Me</nuxt-link>
+            </div>
+            
+            <!-- Portfolio link -->
+            <div class="text-slate-400 md:w-1/2">
+                <nuxt-link class="text-xs md:text-base font-semibold" to="/portfolio">Portfolio</nuxt-link>
+            </div>
+        
+            <!-- Resume link -->
+            <div class="text-slate-400 md:w-1/2">
+                <nuxt-link class="text-xs md:text-base font-semibold" to="/resume">Resume</nuxt-link>
+            </div>
+        </div>
     
-    <!-- Bottom line -->
-    <div class="container mx-auto border-t p-3 border-slate-900 md:px-12"/>
-  </header>
+        <!-- Bottom line -->
+        <div class="container mx-auto border-t p-3 border-slate-900 md:px-12"/>
+    </header>
 </template>
