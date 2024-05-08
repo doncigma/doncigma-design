f7a9637907bda4d0fcb6907af8e88a3986362aaf
Elijah Johnson <astroboy772@gmail.com>
2024-05-07 22:17:23 -0700

    home done fr this time

--- pages/index.vue
+++ pages/index.vue
@@ -5,50 +5,42 @@
         <p class="flex justify-center text-center text-5xl text-slate-400 font-semibold">Design.</p>
     </div>
 
-    <div class="flex flex-row justify-between">
-    
-        <div class="bg-gray-950 w-0 md:w-0"/>
-    
-        <!-- Whole middle -->
-        <div class="flex justify-center bg-gradient-to-t from-slate-900 to-mexicanred-500">
-            <div class="flex flex-col md:w-2/4">
-                <!-- Left image -->
-                <div class="overflow-hidden m-4 border-4 border-slate-300">
-                    <NuxtImg src="home/ai-laptop.jpg" class="w-full"/>
-                </div>
+    <!-- Whole middle -->
+    <div class="flex justify-center bg-gradient-to-t from-slate-900 to-mexicanred-500">
+        <div class="flex flex-col md:w-1/2">
+            <!-- Left image -->
+            <div class="overflow-hidden m-4 border-4 border-slate-300">
+                <NuxtImg src="home/ai-laptop.jpg" class="w-full"/>
+            </div>
 
-                <!-- Company desc -->
-                <div class=" bg-black m-4 p-4">
-                    <p class="bg-black w-full text-lg md:text-xl text-slate-400 
-                        text-justify p-4 border-2 border-slate-800"
-                    >
-                        <span class="text-mexicanred-500">Doncigma</span>
-                        <span class="text-slate-300">Design</span> 
-                        provides 
-                        <span class="text-slate-200 font-semibold">custom web design </span>
-                        and 
-                        <span class="text-slate-200 font-semibold">development services</span> 
-                        to help you create a website that is 
-                        <span class="text-slate-200 font-semibold">unique</span>, 
-                        <span class="text-slate-200 font-semibold">modern</span>, 
-                        and most importantly, a 
-                        <span class="text-slate-200 font-semibold">representation of you</span>. 
-                        
-                        <br><br>
-                        Put the best part of you forward,
-                        <span class="text-mexicanred-500">Doncigma</span>
-                        <span class="text-slate-300">Design</span>.
-                    </p>
-                </div>
+            <!-- Company desc -->
+            <div class=" bg-black m-4 p-4">
+                <p class="bg-black w-full text-lg md:text-xl text-slate-400 
+                    text-justify p-4 border-2 border-slate-800"
+                >
+                    <span class="text-mexicanred-500">Doncigma</span>
+                    <span class="text-slate-300">Design</span> 
+                    provides 
+                    <span class="text-slate-200 font-semibold">custom web design </span>
+                    and 
+                    <span class="text-slate-200 font-semibold">development services</span> 
+                    to help you create a website that is 
+                    <span class="text-slate-200 font-semibold">unique</span>, 
+                    <span class="text-slate-200 font-semibold">modern</span>, 
+                    and most importantly, a 
+                    <span class="text-slate-200 font-semibold">representation of you</span>. 
+                    
+                    <br><br>
+                    Put the best part of you forward,
+                    <span class="text-mexicanred-500">Doncigma</span>
+                    <span class="text-slate-300">Design</span>.
+                </p>
+            </div>
 
-                <!-- Right image -->
-                <div class="overflow-hidden m-4 border-4 border-slate-300">
-                    <NuxtImg src="home/ai-laptop-guy.jpg" class="w-full"/>
-                </div>
+            <!-- Right image -->
+            <div class="overflow-hidden m-4 border-4 border-slate-300">
+                <NuxtImg src="home/ai-laptop-guy.jpg" class="w-full"/>
             </div>
         </div>
-
-        <div class="bg-gray-950 w-0 md:w-0"/>
-    
     </div>
 </template>
